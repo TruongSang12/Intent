70766a6ced6d767749e7a4d64ee3a6ff8b0a41a2
TruongSang12 <truongsang30@gmail.com>
2022-04-03 10:05:38 +0700

    First

---
+++ Intent_TH29-3/app/src/main/java/com/cntt61/intent/sang/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cntt61.intent.sang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
